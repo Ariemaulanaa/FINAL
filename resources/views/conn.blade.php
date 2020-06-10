acdb13d77dcd0c674cd2860ab2c5699cfd0b3522
Arie Maulana <arie.m@mhs.unsyiah.ac.id>
2020-06-10 22:38:13 +0700

    coon

---
+++ resources/views/conn.blade.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+//database location
+$database = "FINAL";
+
+// create Connection
+$conn = new mysqli($servername, $username, $password, $database);
+?>
